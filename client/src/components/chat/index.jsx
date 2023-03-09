5a014391afa1728c4c3689a026afcf2db6fe2718
Darshil Marathe <99969644+DarshilMarathe@users.noreply.github.com>
2023-03-10 02:52:08 +0530

    src import -Standard Practice added

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useMultiChatLogic,MultiChatSocket,MultiChatWindow } from 'react-chat-engine-advanced';
+
+
+const Chat = () => {
+  return (
+    <div>
+      Chat
+    </div>
+  )
+}
+
+export default Chat
